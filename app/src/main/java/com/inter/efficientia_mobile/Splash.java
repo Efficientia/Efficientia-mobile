e1997c30d9dbc7807411d31bc08e8e209e01bf6f
buscariolo-rogerio <buscariolorogerio@gmail.com>
2026-09-03 12:58:59 -0300

    feat/EFFICIENTI-63/adiciona-logica-splash-screen

---
+++ app/src/main/java/com/inter/efficientia_mobile/Splash.java
@@ -0,0 +1,27 @@
+package com.inter.efficientia_mobile;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Segura a splash por 2 segundos, depois vai para a tela de login geral
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
