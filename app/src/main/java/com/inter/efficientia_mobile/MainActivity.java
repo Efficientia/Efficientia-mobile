bf197e29f7ec09784371f0227738acec48efc582
buscariolo-rogerio <buscariolorogerio@gmail.com>
2026-09-03 10:51:41 -0300

    feat/EFFICIENTI-63/implementa-tela-login-inicial

--- app/src/main/java/com/inter/efficientia_mobile/MainActivity.java
+++ app/src/main/java/com/inter/efficientia_mobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.inter.efficientia_mobile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_login_geral);
+
+        // Localiza os botões pelo ID definido no XML
+        Button btnMotorista = findViewById(R.id.btnMotorista);
+        Button btnAdministrador = findViewById(R.id.btnAdministrador);
+
+        // Clique no botão Motorista
+        btnMotorista.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Selecionado: Motorista", Toast.LENGTH_SHORT).show();
         });
-        System.out.println("teste");
+
+        // Clique no botão Administrador
+        btnAdministrador.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Selecionado: Administrador", Toast.LENGTH_SHORT).show();
+        });
     }
 }
