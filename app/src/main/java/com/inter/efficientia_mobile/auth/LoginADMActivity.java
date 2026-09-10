be3a4215c48540b2f7bbbf712aaf0e383761dfed
LocShore <knightlocs@gmail.com>
2026-09-10 12:25:49 -0300

    feat/EFFICIENTI-63/add-admin-login-screen

---
+++ app/src/main/java/com/inter/efficientia_mobile/auth/LoginADMActivity.java
@@ -0,0 +1,33 @@
+package com.inter.efficientia_mobile.auth;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.inter.efficientia_mobile.R;
+
+public class LoginADMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_adm);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText editTextEmail = findViewById(R.id.et_cnpj);
+        EditText editTextSenha = findViewById(R.id.et_senha);
+        Button buttonAvancar = findViewById(R.id.btn_avancar);
+
+    }
+}
