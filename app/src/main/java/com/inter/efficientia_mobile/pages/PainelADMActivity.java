1410de1ee16f319997e7fecdba4c02a8ec5c83fa
LocShore <knightlocs@gmail.com>
2026-09-15 08:31:00 -0300

    feat: add PainelADMActivity and update navigation layout

--- app/src/main/java/com/inter/efficientia_mobile/pages/PainelADMActivity.java
+++ app/src/main/java/com/inter/efficientia_mobile/pages/PainelADMActivity.java
@@ -1,9 +1,6 @@
-package com.inter.efficientia_mobile.auth;
+package com.inter.efficientia_mobile.pages;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,28 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.inter.efficientia_mobile.R;
-import com.inter.efficientia_mobile.main.MainADMActivity;
 
-public class LoginADMActivity extends AppCompatActivity {
+public class PainelADMActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_adm);
+        setContentView(R.layout.activity_painel_adm);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        EditText editTextCNPJ = findViewById(R.id.et_cnpj);
-        EditText editTextSenha = findViewById(R.id.et_senha);
-        Button buttonAvancar = findViewById(R.id.btn_avancar);
-
-        buttonAvancar.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginADMActivity.this, MainADMActivity.class);
-            startActivity(intent);
         });
     }
 }
